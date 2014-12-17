769af1350e42ff04390ef157b2d9576af4663db1
Kenneth Bruce Uy <missingno15@gmail.com>
2014-12-17 00:33:41 -0500

    edit fibonacci instructions

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -3,5 +3,19 @@
 # Create a Fibonacci class that has a method, #is_fibonacci? which takes a
 # single number as an argument and returns true or false depending on whether
 # or not it belongs in the Fibonacci sequence.
-
-
+#
+# Look online on if you don't know or don't remember
+# what the Fibonacci sequence is, read how it is 
+# implemented, then turn the algorithm into Ruby code. 
+# Try not to look at already completed answers 
+# online. While looking at other people's answers 
+# is encouraged, its much more beneficial to try it out 
+# youself before looking at other answers. 
+# 
+# Search the Ruby documentation for any interesting methods 
+# that could help you implement the Reverse Polish Notation 
+# algorithm. Learning to get used to documentation is skill 
+# important to a developer. To get you started, you can 
+# start looking up the documentation in the following link:
+# 
+# http://ruby-doc.org/core-2.1.5/doc/syntax/methods_rdoc.html
